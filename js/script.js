36d064f4df027d2a45d685e743a86c1ca55aa340
Luana Lima da Silva Martins <luanalima.silva20147@gmail.com>
2026-05-06 15:50:08 -0300

    projeto fase final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const entrar = document.querySelector('.btn-entrar')
 const toggle = document.getElementById('toggle-theme')
+const ctx = document.getElementById('grafico')
 
 if (entrar) {
     entrar.addEventListener('click', function(event) {
@@ -9,9 +10,37 @@ if (entrar) {
     })
 }
 
-
 if(toggle) {
     toggle.addEventListener('click', () => {
         document.body.classList.toggle('dark')
+    })
+}
+
+if (ctx) {
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai'],
+            datasets: [
+                {
+                    label: 'Receitas',
+                    data: [5000, 6000, 5500, 7000, 8000],
+                    backgroundColor: '#adebad'
+                },
+                {
+                    label: 'Despesas',
+                    data: [3000, 3500, 4000, 4500, 5000],
+                    backgroundColor: '#ffb3b3'
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top'
+                }
+            }
+        }
     })
 }
